d055c9fda05e6641c63a4d076c0354dc6fcd86e7
Sara Prager <sari.prager@gmail.com>
2021-11-16 17:30:43 +0200

    Better solution for groupAnagrams

--- algorithms/groupAnagrams.js
+++ algorithms/groupAnagrams.js
@@ -1,16 +1,48 @@
 // https://leetcode.com/problems/group-anagrams/
 
-const groupAnagrams = (strs) => {
+// Solution #1: O(m*nlogn)
+const groupAnagramsV1 = (strs) => {
+  const result = {};
+
+  strs.forEach(str => {
+      const key = [...str].sort().join('');
+      if (result[key]) {
+        result[key].push(str);
+      } else {
+        result[key] = [str];
+      }
+  });
+  
+  return Object.values(result)
+};
+
+// Solution #2: O(mn)
+const LETTERS_COUNT = 26;
+const FIRST_VALID_CHAR = 'a';
+
+const groupAnagramsV2 = (strs) => {
     const result = {};
 
     strs.forEach(str => {
-        const key = [...str].sort().join('');
+        const key = buildKey(str);
         if (result[key]) {
           result[key].push(str);
         } else {
           result[key] = [str];
         }
     });
-
+    
     return Object.values(result)
+};
+
+const buildKey = (str) => {
+  const lettersCount = new Array(LETTERS_COUNT).fill(0);
+
+  [...str]
+  .forEach(strChar => {
+      const charCode = strChar.charCodeAt(0) - FIRST_VALID_CHAR.charCodeAt(0);
+      lettersCount[charCode]++
+  });
+
+  return lettersCount;
 };
